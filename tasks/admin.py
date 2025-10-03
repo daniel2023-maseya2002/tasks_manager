2d648e7230f7a6d2be7b1b96e10fa01037a2dbdd
daniel2023-maseya2002 <maseyadaniel@gmail.com>
2025-10-04 00:22:47 +0200

    created admin can create user and assign the user role and tasks

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import CustomUser, Task
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ("Role", {"fields": ("role",)}),
+    )
+    list_display = ("username", "email", "role", "is_staff", "is_superuser")
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("title", "owner", "assigned_to", "due_date", "is_completed")
+    list_filter = ("is_completed", "due_date")
+    search_fields = ("title", "description")
--- tasks/models.py
+++ tasks/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth import get_user_model
 
 # Create your models here.
 class CustomUser(AbstractUser):
@@ -11,3 +12,35 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return f"{self.username} ({self.role})"
+    
+CustomUser = get_user_model()
+
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.DateField(null=True, blank=True)
+    due_date = models.DateField(null=True, blank=True)
+    is_completed = models.BooleanField(default=False)
+
+    #Relation
+
+    owner = models.ForeignKey(
+        CustomUser,
+        related_name="owned_tasks",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    ) # user who created it
+
+    assigned_to = models.ForeignKey(
+        CustomUser,
+        related_name="assigned_tasks",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    ) # optional: admin assigns
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title} (Owner: {self.owner.username})"
